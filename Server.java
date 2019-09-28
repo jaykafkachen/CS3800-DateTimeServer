6e870b80c239b61a745601d0c63f3e5f5d24abcf
Jay C <jaykafkachen@gmail.com>
2019-09-28 14:46:02 -0700

    server & client functional

--- Server.java
+++ Server.java
@@ -28,9 +28,8 @@ class Server
 			
 			String packet = in.readLine();
 			
-			sv.printRequest(packet);
+			//sv.printRequest(packet);
 
-			
 			String str = sv.parseRequest(packet);
 
 			out.writeBytes(str);          
@@ -53,29 +52,78 @@ class Server
 		return packet;
 	}
 
-	public String parseRequest(String packet)
+	public String parseRequest(String packet) throws IOException
 	{
-		String msg = "HTTP/1.0 200 OK\r\nContent-Length: 120\r\nContent-Type: text/html\r\n\r\n\r\n<html>\n\n";
+		String msg = "HTTP/1.0 200 OK\r\nContent-Length: 144\r\nContent-Type: text/html\r\n\r\n\r\n<html>\n\n";
 		String coda = "\n</html>\n";
-
+		TimeClient tc = new TimeClient();
+		String response = tc.getTime();
+		String gmt = convertTime("GMT", response);
+		String est = convertTime("EST", response);
+		String pst = convertTime("PST", response);
 		if(packet.contains("GET /time?zone=all"))
 		{
-			
+			msg += gmt+"<p>"+est+"<p>"+pst;
 		}
 		else if(packet.contains("GET /time?zone=est"))
 		{
-
+			msg+=est;
 		}
 		else if(packet.contains("GET /time?zone=pst"))
 		{
-
+			msg+=pst;
 		}
 		else
 		{
 			String inv = "Invalid request";
-			msg.append(inv+coda);
+			msg+= inv+coda;
 		}
 		return msg;
 	}
 
+	public String convertTime(String zone, String timemsg)
+	{
+		if(!(zone.equals("GMT") || zone.equals("PST") || zone.equals("EST")))
+			return "invalid date/time zone input\n\n";
+
+		String[] utcarr = timemsg.split(" ");
+		String datetime = "20"+utcarr[1]+"T"+utcarr[2];
+		String dst = utcarr[3];
+		
+		String prefix = zone + " Date/Time: ";
+
+		LocalDateTime time = LocalDateTime.parse(datetime);
+		time = daylight(zone,dst,time);
+		datetime = time.getMonthValue()+"/"+time.getDayOfMonth()+"/"+time.getYear()+", ";
+		
+		int hr = time.getHour();
+		String ampm = "AM";
+		if(hr==0)
+			hr=12;
+		else if(hr>12)
+		{
+			hr-=12;
+			ampm="PM";
+		}
+		datetime+=hr+":"+time.getMinute()+" "+ampm;
+		
+		return prefix + datetime;
+	}
+
+	public LocalDateTime daylight(String zone, String dst, LocalDateTime gmt)
+	{
+		if(zone.equals("GMT"))
+			return gmt;
+		int estoffset = 5;
+		int pstoffset = 8;
+		if(!dst.equals("00"))
+		{
+			estoffset--;
+			pstoffset--;	
+		}
+		if(zone.equals("EST"))
+			return gmt.minusHours(estoffset);
+		else	
+			return gmt.minusHours(pstoffset);
+	}
 }
--- TimeClient.java
+++ TimeClient.java
@@ -3,11 +3,43 @@ import java.io.*;
 
 public class TimeClient 
 { 
-	private Socket socket		 = null; 
-	private DataInputStream input = null;
-   
+	//private Socket socket		 = null; 
+	//private DataInputStream input = null;
 	
-	public static void main(String args[]){ 
-		TimeClient client = new TimeClient("time.nist.gov");		
+	String time = "time.nist.gov"; 
+	int port = 13;
+
+	public TimeClient() {	}
+	public static void main(String args[]) throws Exception
+	{ 
+		//TimeClient tc = new TimeClient();	
+		//String timemsg = tc.getTime();
 	} 
+
+	public String getTime() throws IOException
+	{
+		InetAddress timeIP = InetAddress.getByName(time);
+		Socket client = new Socket(timeIP, port);	
+		char[] buffer = new char[32];   //buffer large enough to grab the 1st 4 values for date/time
+									    //all other values are irrelevant for the date/time response
+		try
+		{	
+			DataOutputStream out = new DataOutputStream(client.getOutputStream());
+			BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			String rq = "get the date"; //DAYTIME protocol will ignore data in request message
+			out.writeBytes(rq);         //send TCP request to time.nist.gov
+			out.flush();
+			in.read(); 				    //consumes first newline \n before date msg
+			in.read(buffer);			//reads time response to buffer[]
+			out.close();
+
+		} catch(IOException e)
+		{	
+			e.printStackTrace();
+			buffer = ("time not found").toCharArray();	
+		}
+
+		client.close();
+		return new String(buffer);
+	}
 } 
